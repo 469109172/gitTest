d358961ec57a9ae5b3757e6fe721a4434e186717
469109172 <469109172@qq.com>
2016-01-02 21:55:17 +0800

    Update MainActivity.java
    增加另外一条log

--- app/src/main/java/com/hongxi/gittest/MainActivity.java
+++ app/src/main/java/com/hongxi/gittest/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e("000000000000000000000","-----------------");
+        Log.e("000000000000000000000","-----------------");
     }
 
     @Override
